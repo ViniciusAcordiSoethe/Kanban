69456ce2c2f04a55c75e6790457dd4e5b85915bc
Vinicius Acordi Soethe <soethe42@gmail.com>
2022-05-05 02:17:17 -0300

    Augumas atualizações no Vue

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,48 @@
 <script>
+export default{
+  data(){
+    return{
+      conteudos:[
+        {conteudoid:"afazer" , titulo:"A Fazer",
+        cards:[{
+        id:1 , nome: "Card 1 a fazer" , cont:"Conteudo do card 1",
+        id:2 , nome: "Card 2 a fazer" , cont:"Conteudo do card 2",
+        }]
+        },
+        {conteudoid:"feito" , titulo:"Feito",
+        cards:[{
+        id:3 , nome: "Card 1 Feito" , cont:"Conteudo do card 1",
+        id:4 , nome: "Card 2 Feito" , cont:"Conteudo do card 2",
+        }]
+        },
+      ]
+    }
+  },
+};
 </script>
-
 <template>
-    <section id="kanbanGrande">
-        <h1>Kanban</h1>
-       <div id="kanbanPrincipal">
-
-          <div class="kanbanColuna">
-              <h1>a Fazer</h1>
-              <div class="card">
-                Card 1 
-                <br>
-                <p>COnteudo</p>
-                <button>Proximo</button>
-              </div>
-              <div class="card">
-                Card 2 
-                <br>
-                <p>COnteudo</p>
-                <button>Proximo</button>
-              </div>
-          </div>
-          <div class="kanbanColuna">
-              <h1>Feito</h1>
-              <div class="card">
-                Card 1 
-                <br>
-                <p>COnteudo</p>
-                <button>Anterior</button>
-              </div>
-              <div class="card">
-                Card 2 
-                <br>
-                <p>COnteudo</p>
-                <button>Anterior</button>
-              </div>
-          </div>
-       </div>
-    </section>
+  <section id="kanbanGrande">
+    <h1>Kanban</h1>
+    <div id="kanbanPrincipal">
+      <div class="kanbanColuna" v-for="conteudo in conteudos" :key="conteudo.conteudo">
+        <h1>{{conteudo.titulo}}</h1>
+        <div class="card" v-for="card in conteudo.cards" :key="card.id">
+          {{card.nome}}
+          <br />
+          <p>{{card.cont}}</p>
+          <button>Proximo</button>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
-
 <style>
-body , h1 ,html{
-  margin : 0px;
+body,
+h1,
+html {
+  margin: 0px;
 }
-#kanbanGrande{
+#kanbanGrande {
   background-color: aqua;
   width: 100vw;
   height: 100vh;
@@ -54,7 +52,7 @@ body , h1 ,html{
   justify-content: center;
   align-items: center;
 }
-#kanbanPrincipal{
+#kanbanPrincipal {
   border: solid black;
   background-color: blueviolet;
   display: flex;
@@ -63,12 +61,12 @@ body , h1 ,html{
   width: 80%;
   height: 40vh;
 }
-.kanbanColuna{
+.kanbanColuna {
   border: solid black;
   width: 45%;
   text-align: center;
 }
-.card{
+.card {
   border: solid black;
   margin: 0px 50px 5px 50px;
 }
