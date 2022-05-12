c6d56d752512c2faa27bae54535ef052d846eb10
Vinicius Acordi Soethe <soethe42@gmail.com>
2022-05-11 21:37:20 -0300

    Update app

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 <template>
     <nav>
       <Router-link to="/">Home</Router-link>
-      <Router-link to="/sobre">sobre</Router-link>
+      <Router-link to="/sobre">Sobre</Router-link>
     </nav>
   <Router-view />
 </template>
